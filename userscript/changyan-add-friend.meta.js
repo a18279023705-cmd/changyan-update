7ad511f96f877e989db0737831347f607653ee36
a18279023705-cmd <a18279023705@gmail.com>
2026-06-27 08:59:39 +0800

    userscript meta v10.0.0

--- userscript/changyan-add-friend.meta.js
+++ userscript/changyan-add-friend.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         畅言加好友 阿陌专用 后台稳定版
 // @namespace    http://tampermonkey.net/
-// @version      9.20.18
-// @description  畅言加好友阿陌专用，填号后只模拟回车搜索
+// @version      10.0.0
+// @description  畅言加好友阿陌专用，v2.3填号回车逻辑+面板频繁限制
 // @match        *://web.rvtqh.com/*
 // @require      https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js
 // @grant        none
