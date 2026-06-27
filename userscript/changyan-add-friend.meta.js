2573643877e397abe0c70d9648225293a5dfd9bc
a18279023705-cmd <a18279023705@gmail.com>
2026-06-27 10:26:08 +0800

    userscript meta v10.2.2

--- userscript/changyan-add-friend.meta.js
+++ userscript/changyan-add-friend.meta.js
@@ -1,18 +1,14 @@
 // ==UserScript==
 // @name         畅言加好友 阿陌专用 后台稳定版
 // @namespace    http://tampermonkey.net/
-// @version      10.2.1
-// @description  畅言加好友，GM注入面板+本地可装
+// @version      10.2.2
+// @description  畅言加好友，grant none 最大兼容
 // @match        *://web.rvtqh.com*
 // @match        *://*.rvtqh.com/*
 // @match        http://web.rvtqh.com/*
 // @match        https://web.rvtqh.com/*
-// @grant        GM.addElement
-// @grant        GM.setValue
-// @grant        GM.getValue
-// @inject-into  page
-// @run-at       document-start
-// @connect      *
+// @grant        none
+// @run-at       document-end
 // @homepageURL  https://github.com/a18279023705-cmd/changyan-update
 // @updateURL    https://raw.githubusercontent.com/a18279023705-cmd/changyan-update/main/userscript/changyan-add-friend.meta.js
 // @downloadURL  https://github.com/a18279023705-cmd/changyan-update/releases/latest/download/changyan-add-friend.user.js
